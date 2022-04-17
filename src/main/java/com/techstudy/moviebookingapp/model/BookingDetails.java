dfaabb3fdee2d1e13bf08c3a574b2b0d78f770e4
Souvik Dey <souvikdey@technoexponent.com>
2022-04-17 19:18:38 +0400

    code modified

--- src/main/java/com/techstudy/moviebookingapp/model/BookingDetails.java
+++ src/main/java/com/techstudy/moviebookingapp/model/BookingDetails.java
@@ -8,7 +8,6 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
